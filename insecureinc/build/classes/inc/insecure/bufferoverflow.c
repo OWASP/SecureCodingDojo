b839c24a4fede3ce623f1826463c1222cc4dcc3e
Paul Ionescu (RD-CA) <paulgin@gmail.com>
2017-11-23 16:23:16 -0500

    Upload a c file for a challenge

--- insecureinc/build/classes/inc/insecure/bufferoverflow.c
+++ insecureinc/build/classes/inc/insecure/bufferoverflow.c
@@ -2,17 +2,29 @@
 #include <string.h>
 
 int main(){
-	char MASTER_PASSWORD[9]="59563376";
-	char userPass[9];
+        char MASTER_PASSWORD[9]="59563376";
+        char userPass[9];
+        int i;
 
-	printf("Enter the master password:\n");
-	gets(userPass);
+        printf("Enter the master password:\n");
+        gets(userPass);
 
-	if(strcmp(userPass,MASTER_PASSWORD)==0){
-		printf("PASSWORD VERIFIED\n");
-	}
-	else{
-		printf("Invalid password!\n");
-	}
-	return 0;
+        printf("\nuserPass address: '%p'",&userPass);
+        printf("\nuserPass contents:\n|");
+        for(i=0;i<9;i++){
+                printf("%02x|",userPass[i]);
+        }
+        printf("\n\nMASTER_PASSWORD address: '%p'",&MASTER_PASSWORD);
+        printf("\nMASTER_PASSWORD contents:\n|");
+        for(i=0;i<9;i++){
+                printf("%02x|",MASTER_PASSWORD[i]);
+        }
+
+        if(strcmp(userPass,MASTER_PASSWORD)==0){
+                printf("\n\nPASSWORD VERIFIED\n");
+        }
+        else{
+                printf("\n\nInvalid password!\n");
+        }
+        return 0;
 }
