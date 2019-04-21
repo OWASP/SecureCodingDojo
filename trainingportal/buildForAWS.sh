3509b3d593f5fc74a44ffb49d3a7e3c805760035
Paul Ionescu (RD-CA) <paulgin@gmail.com>
2019-04-20 21:56:19 -0400

    Fix a concurrency issue with the db tests

--- trainingportal/buildForAWS.sh
+++ trainingportal/buildForAWS.sh
@@ -1,3 +1,7 @@
+set -o errexit
+npm test
+cp prod.config.js config.js;
 zip SCD-AWSBuild-"$(date)".zip -r .ebextensions/* accountWhitelist.json aescrypto.js auth.js challenges.js \
-challengeSecrets.json config.js db.js node_modules/* package-lock.json package.json public/* static/* SquadDirectory.csv \
- server.js util.js -x "*.DS_Store"
+challengeSecrets.json config.js db.js node_modules/* package-lock.json package.json public/* static/* \
+ server.js util.js -x "*.DS_Store";
+cp dev.config.js config.js
