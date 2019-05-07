ccaf57a81340c9ff50a194ee8966e5811c31bfd4
Paul Ionescu (RD-CA) <paulgin@gmail.com>
2019-05-06 20:22:59 -0400

    Add sqlite support

---
+++ trainingportal/dbsetup.sqlite.sql
@@ -0,0 +1,19 @@
+CREATE TABLE challengeEntries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId INTEGER,
+  challengeId CHAR(255)
+);
+CREATE TABLE teams (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name CHAR(255) UNIQUE,
+  description CHAR(2048),
+  ownerId INTEGER UNIQUE
+);
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  accountId CHAR(255) NOT NULL UNIQUE,
+  teamId INTEGER unsigned,
+  level INTEGER,
+  familyName CHAR(255),
+  givenName CHAR(255)
+);
