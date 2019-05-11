62147c520dd146514444ecf27161c208d12062b5
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-05-11 14:45:43 -0400

    Docker image support for training portal

--- trainingportal/buildForAWS.sh
+++
@@ -1,7 +0,0 @@
-set -o errexit
-npm test
-cp prod.config.js config.js;
-zip SCD-AWSBuild-"$(date)".zip -r .ebextensions/* accountWhitelist.json aescrypto.js auth.js challenges.js \
-challengeSecrets.json config.js db.js node_modules/* package-lock.json package.json public/* static/* \
- server.js util.js -x "*.DS_Store";
-cp dev.config.js config.js
--- trainingportal/server.js
+++ trainingportal/server.js
@@ -422,6 +422,10 @@ app.get('/api/report',  (req, res) => {
 
 db.init();
 
+process.on('SIGINT', function() {
+  process.exit();
+});
+
 app.listen(8081,function(){
     util.log('Listening on 8081');
     util.log('Configured url:'+config.dojoUrl);
