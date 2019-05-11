7806a972ebc26846ddee8388e9a7c4ff9a4d2a82
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-05-11 17:22:40 -0400

    Enable docker config with google auth and badgr

--- build/trainingportal/config.js
+++ build/trainingportal/config.js
@@ -6,11 +6,15 @@ config.isSecure = config.dojoUrl.startsWith("https");
 config.playLinks = [{"challengeid":"links"}];
 
 //account id whitelisting rules, if undefined all accounts are allowed
-config.allowedAccountPattern = /^Local_/; //use in conjunction with account whitelist to enable accounts matching the pattern e.g. Local_
+config.allowedAccountPattern = /^Local_|\d+/; //use in conjunction with account whitelist to enable accounts matching the pattern e.g. Local_
 
 config.dataDir = process.env.DATA_DIR; //use to specify where the local db or the local users will be stored
 
 config.localUsersPath = 'localUsers.json';//authentication using a password file containing users and hashed passwords, no lockout
 
-module.exports = config;
+config.googleOauthCallbackUrl = config.dojoUrl+"/public/google/callback";
+config.googleClientId = process.env.GOOGLE_CLIENT_ID; //enable to configure Google authentication
+config.encGoogleClientSecret = process.env.ENC_GOOGLE_CLIENT_SECRET; 
+config.encBadgrToken = process.env.ENC_BADGR_TOKEN; 
 
+module.exports = config;
