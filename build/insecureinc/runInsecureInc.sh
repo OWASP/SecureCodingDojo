69155bd071dfc9cf5e3d826304e2237eaf287494
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-05-11 20:24:15 -0400

    Scripts to run insecure.inc and trainingportal containers

---
+++ build/insecureinc/runInsecureInc.sh
@@ -0,0 +1,3 @@
+docker run -p 8080:8080 \
+-e CHALLENGE_MASTER_SALT=$CHALLENGE_MASTER_SALT \
+ securecodingdojo/insecure.inc
---
+++ build/trainingportal/runTrainingPortal.sh
@@ -0,0 +1,12 @@
+docker run -p 8081:8081 \
+-e DOJO_URL=http://localhost:8081 \
+-e DOJO_TARGET_URL=$DOJO_TARGET_URL \
+-e DATA_DIR=/dojofiles \
+-e GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID \
+-e ENC_GOOGLE_CLIENT_SECRET=$ENC_GOOGLE_CLIENT_SECRET \
+-e ENC_BADGR_TOKEN=$ENC_BADGR_TOKEN \
+-e ENC_KEY=$ENC_KEY \
+-e ENC_KEY_IV=$ENC_KEY_IV \
+-e CHALLENGE_MASTER_SALT=$CHALLENGE_MASTER_SALT \
+--volume=/$DATA_DIR:/dojofiles:consistent \
+securecodingdojo/trainingportal
