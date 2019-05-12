64dfbccbd7b3cf01a57bb3659b51a32a99cd88a3
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-05-11 20:53:51 -0400

    Allow configuring challenge links via env variable

--- build/trainingportal/config.js
+++ build/trainingportal/config.js
@@ -3,7 +3,7 @@ config.dojoUrl = process.env.DOJO_URL;
 config.dojoTargetUrl = process.env.DOJO_TARGET_URL;
 config.isSecure = config.dojoUrl.startsWith("https");
 //for those cases when absolute links are specific to your environment
-config.playLinks = [{"challengeid":"links"}];
+config.playLinks = JSON.parse(process.env.CHALLENGE_URLS); //[{"challengeid":"links"}];
 
 //account id whitelisting rules, if undefined all accounts are allowed
 config.allowedAccountPattern = /^Local_|\d+/; //use in conjunction with account whitelist to enable accounts matching the pattern e.g. Local_
