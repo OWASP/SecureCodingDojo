526a82ab6a1c8316afa47bc695b792152a4358c3
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-05-11 20:53:51 -0400

    Allow configuring challenge links via env variable

--- build/trainingportal/runTrainingPortal.sh
+++ build/trainingportal/runTrainingPortal.sh
@@ -8,5 +8,6 @@ docker run -p 8081:8081 \
 -e ENC_KEY=$ENC_KEY \
 -e ENC_KEY_IV=$ENC_KEY_IV \
 -e CHALLENGE_MASTER_SALT=$CHALLENGE_MASTER_SALT \
+-e CHALLENGE_URLS=$CHALLENGE_URLS \
 --volume=/$DATA_DIR:/dojofiles:consistent \
 securecodingdojo/trainingportal
