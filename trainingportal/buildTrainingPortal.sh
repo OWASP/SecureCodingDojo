9d57345b7077686e3db1e063a0db926892f18168
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-06-12 11:12:39 -0400

    Decrypt utility

--- trainingportal/buildTrainingPortal.sh
+++ trainingportal/buildTrainingPortal.sh
@@ -6,10 +6,10 @@ cp prod.config.js config.js;
 rm -f ../build/trainingportal/*.zip
 zip ../build/trainingportal/SCD-"$(date)".DOCKER.zip -r *.js *.sql \
  package-lock.json package.json public/* static/* challengeSecrets.json \
- -x "*.DS_Store" -x "encryptConfigs.js" -x "*.config.js" -x "config.js";
+ -x "*.DS_Store" -x "encryptConfigs.js" -x "decrypt.js" -x "*.config.js" -x "config.js";
 rm -f *.zip 
 zip SCD-"$(date)".AWS.zip -r *.js *.sql \
  .ebextensions/* \
  node_modules/* public/* static/* challengeSecrets.json \
- -x "*.DS_Store" -x "encryptConfigs.js" -x "*.config.js";
+ -x "*.DS_Store" -x "encryptConfigs.js" -x "decrypt.js" -x "*.config.js";
 cp dev.config.js config.js
---
+++ trainingportal/decrypt.js
@@ -0,0 +1,3 @@
+const path = require('path');
+const aescrypto = require(path.join(__dirname, 'aescrypto'));
+console.log(aescrypto.decrypt(process.argv[2]));
