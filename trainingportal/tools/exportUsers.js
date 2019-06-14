abdf5df5af454d8f4f84e2bf2d8efecd22864c1f
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-06-14 07:46:03 -0400

    Script for migrating users

---
+++ trainingportal/tools/exportUsers.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const db = require(path.join(__dirname, '../db'));
+const USER_DUMP = "userDump.json";
+
+var conn = db.getConn();
+async function exportUsers(){
+    var result = [];
+
+    let users = await conn.queryPromise("SELECT id,givenName,familyName,level,teamId FROM users");
+    let count = users.length;
+        
+    for(let user of users){
+        let entries = await conn.queryPromise("SELECT challengeId FROM challengeEntries WHERE userId=?",[user.id]);
+    
+        var passedChallenges = [];
+        if(entries!=null){
+            entries.forEach(function(challenge){
+                passedChallenges.push(challenge.challengeId);
+            });
+        }
+        
+        user.passedChallenges = passedChallenges;
+        result.push(user);
+    }
+
+    fs.writeFileSync(USER_DUMP, JSON.stringify(result), 'utf8');
+    console.log(`Users dumped to ${USER_DUMP}`);
+    process.exit(0);
+}
+
+exportUsers();
+
---
+++ trainingportal/tools/importUsers.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const fs = require('fs');
+const db = require(path.join(__dirname, '../db'));
+const util = require(path.join(__dirname, '../util'));
+const challenges = require(path.join(__dirname, '../challenges'));
+
+const USER_DUMP = "userDump.json";
+
+
+async function importUsers(){
+
+    var accountIdFormat = process.argv[2];
+    var teamId = process.argv[3];
+    
+    
+    if(!fs.existsSync(USER_DUMP)){       
+        console.log(`Cannot find ${USER_DUMP}`);
+    }
+    else if(util.isNullOrUndefined(accountIdFormat)){
+        console.log("User import functionality. Format: node importUsers.js <ACCOUNT_ID_FORMAT> [<TEAM_ID>]");
+        console.log(`Please specify user id format ex:'SAML_%givenName%_%lastName%'`);
+    }
+    else{
+        users = require(path.join(__dirname,USER_DUMP));
+        for(let user of users){
+            user.accountId = accountIdFormat.replace("%givenName%",user.givenName).replace("%familyName%",user.familyName);
+
+            //check if the user exists
+            var dbUser = await db.getPromise(db.getUser, user.accountId);
+           
+
+            if(dbUser===null){ //create the user and update the team id
+                //user doesn't exist level is 0
+                user.level = 0;
+                await db.getPromise(db.insertUser,user);
+                dbUser = await db.getPromise(db.getUser, user.accountId);
+                if(!util.isNullOrUndefined(teamId)){
+                    dbUser.teamId = teamId;
+                }
+            }
+            if(dbUser && (dbUser.level==0 || dbUser.level < user.level)){ //only import challenges for users at a lower level
+
+                for(let chId of user.passedChallenges){
+                    await new Promise((resolve, reject) => {
+                        challenges.insertChallengeEntry(dbUser,{"id":chId,"name":""},(err,result) =>{
+                            if(err) reject(err);
+                            else resolve(result);
+                        })
+                    });
+
+                }
+            }
+        }
+        console.log("Import complete.");
+        process.exit(0);
+    }
+}
+
+importUsers();
