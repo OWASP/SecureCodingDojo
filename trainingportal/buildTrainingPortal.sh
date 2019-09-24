fb167317bbc242f075c19fe107047e0e23a9f358
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2019-09-24 16:56:39 -0400

    Allow running custom test scripts as part of the build

--- trainingportal/buildTrainingPortal.sh
+++ trainingportal/buildTrainingPortal.sh
@@ -1,10 +1,17 @@
 set -o errexit
 npm test
+
+#include here your own test tools
+if [ -f "runCustomTestScripts.sh" ]
+then
+    ./runCustomScripts.sh
+fi
+
 npm install
 npm rebuild
 
 rm -f ../build/trainingportal/*.zip
-zip ../build/trainingportal/SCD-"$(date)".DOCKER.zip -r *.js *.sql \
+zip ../build/trainingportal/SCD-"$(date)".DOCKER.zip -rq *.js *.sql \
  package-lock.json package.json public/* static/* sql/* \
  -x "*.DS_Store" -x "encryptConfigs.js" -x "decrypt.js" -x "*.config.js" -x "config.json";
 
@@ -12,7 +19,7 @@ docker build -t securecodingdojo/trainingportal ../build/trainingportal/
 
 rm -f *.zip 
 cp prod.config.json config.json;
-zip SCD-"$(date)".AWS.zip -r *.js config.json *.sql \
+zip SCD-"$(date)".AWS.zip -rq *.js config.json *.sql \
  .ebextensions/* \
  node_modules/* public/* static/* sql/*  \
  -x "*.DS_Store" -x "encryptConfigs.js" -x "decrypt.js" -x "*.config.js" -x "*.config.json" ;
