fa89d54dacdf2b2a6cfc79c2de9e10e4d678613e
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2020-05-08 12:55:46 -0400

    Add dojo master salt variable to redblueapp

--- redblueapp/buildAndRun.sh
+++ redblueapp/buildAndRun.sh
@@ -1,2 +1,2 @@
 docker build -t securecodingdojo/redblueapp .
-docker run -p 8888:8888 -p 8080:80  securecodingdojo/redblueapp
+docker run -p 8888:8888 -p 8080:80 -e MASTER_SALT="$CHALLENGE_MASTER_SALT" securecodingdojo/redblueapp
