523ecb48c30699d410c576b876395f50f4139a19
Nimesh Jethwa <nimesh.jethwa@live.com>
2020-05-11 13:37:17 -0400

    Build and deploy insecureinc/redblueapp and set a cron

--- pipelines/deploy_app.groovy
+++ pipelines/deploy_app.groovy
@@ -1,19 +1,40 @@
+
 def tfNode = 'tf_node_secure-coding-dojo'
 
 node(tfNode){
+  properties([
+    pipelineTriggers([
+      cron('0 18 * * *') // Run at 12:15am everyday.
+    ])
+  ])
+
   // Global vars
   def awsAccountID = sh(returnStdout: true, script: "aws sts get-caller-identity --query Account --output text").trim()
   def awsRegion = 'us-east-2'
 
-  // ECR vars
+  // ECS/ECR vars
+  def ecsCluster = "arn:aws:ecs:${awsRegion}:${awsAccountID}:cluster/InsecureInc-EC2"
   def ecrRegistry = "${awsAccountID}.dkr.ecr.${awsRegion}.amazonaws.com"
-  def ecrRepoName = "securecodingdojo/redblueapp"
-  def ecrImageTag = "latest"
-  def ecrImageURI = "${ecrRegistry}/${ecrRepoName}:${ecrImageTag}"
-
-  // ECS vars
-  def ecsCluster = "arn:aws:ecs:${awsRegion}:${awsAccountID}:cluster/InsecureInc"
-  def ecsServiceName = "RedBlueApp"
+  
+  def apps = [
+    "insecureinc": [
+      ecrRepoName : "securecodingdojo/insecure.inc",
+      ecrImageTag: "latest",
+      ecsServiceNames: [
+        "InsecureInc"
+      ],
+      dockerfileDir: "build/insecureinc"
+    ],
+    "redblueapp": [
+      ecrRepoName : "securecodingdojo/redblueapp",
+      ecrImageTag: "latest",
+      ecsServiceNames: [
+        "RedBlueApp",
+        "RedBlueAppNetworkDiag"
+      ],
+      dockerfileDir: "redblueapp"
+    ]
+  ]
 
   try {
     stage("Checkout source"){
@@ -23,13 +44,16 @@ node(tfNode){
 
     stage("Build image and push to ECR") {
       echo "Build and push image to ECR..."
-      sh(returnStdout: true, script: "aws ecr get-login --no-include-email --region '${awsRegion}' --registry-ids '${awsAccountID}'")
-      dir("redblueapp"){
-        docker.withRegistry("https://${ecrRegistry}") {
-          dockerImage = docker.build(ecrImageURI, "-f Dockerfile .")
-          dockerImage.push()
-        }
-      }
+      sh(returnStdout: true, script: "\$(aws ecr get-login --no-include-email --region '${awsRegion}' --registry-ids '${awsAccountID}')")
+      apps.each { item ->
+        echo "Building and pushing ${item.key}"
+        dir(apps["${item.key}"]["dockerfileDir"]){
+          docker.withRegistry("https://${ecrRegistry}") {
+            dockerImage = docker.build("${ecrRegistry}/${apps["${item.key}"]["ecrRepoName"]}:${apps["${item.key}"]["ecrImageTag"]}", "-f Dockerfile .")
+            dockerImage.push()
+          } // docker.withRegistry
+        } // dir
+      } // apps.each
     } // stage
 
     stage("Deploy to ECS"){
@@ -40,7 +64,12 @@ node(tfNode){
       // (for example, my_image:latest ), you do not need to create a new revision of your task definition. You can update 
       // the service using the force-new-deployment option. The new tasks launched by the deployment pull the current image/tag 
       // combination from your repository when they start.
-      sh(returnStdout: true, script: "aws ecs --region ${awsRegion} update-service --cluster ${ecsCluster} --service ${ecsServiceName} --force-new-deployment")
+      apps.each { item ->
+        apps["${item.key}"]["ecsServiceNames"].each { service ->
+          sh(returnStdout: true, script: "aws ecs --region ${awsRegion} update-service --cluster ${ecsCluster} --service ${service} --force-new-deployment")
+          echo "Deployed ${service}"
+        } // service.each
+      } // apps.each
     } // stage
   } // try
   finally {
