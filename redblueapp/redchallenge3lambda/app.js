e42a4d18c6786e58845f8bc80f934da121b606f0
Paul Ionescu (RD-CA) <paul_ionescu@trendmicro.com>
2020-05-13 16:19:16 -0400

    Add flags for red ch3 and blue ch3

--- redblueapp/redchallenge3lambda/app.js
+++ redblueapp/redchallenge3lambda/app.js
@@ -5,13 +5,18 @@ exports.challengeValidator = function (event, context) {
         return context.fail("Invalid salt");
     }
 
-    const challengeId = "red_ch3";
+    let challengeId = "red_ch3";
+    
+    if(event.team==="blue"){
+        challengeId = "blue_ch3";
+    }
+
     let masterSalt;
 
     if (process.env.CHALLENGE_MASTER_SALT) {
         masterSalt = process.env.CHALLENGE_MASTER_SALT;
     } else {
-        context.fail("Expected CHALLENGE_MASTER_SALT env variable to be set.")
+        context.fail("Expected CHALLENGE_MASTER_SALT env variable to be set.");
     }
 
     var verificationHash = crypto.createHash('sha256').update(challengeId + event.salt + masterSalt).digest('base64');
