dbc819a48b3f401c5438e538f308fbf33cd72f64
mEEksIsu <mhsisu@gmail.com>
2020-05-13 11:48:00 -0400

    Adds lambda to validate vulnerableapp challenge 3 code

---
+++ redblueapp/redchallenge3lambda/app.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+exports.challengeValidator = function (event, context) {
+    if (!event.salt || event.salt.length < 5) {
+        return context.fail("Invalid salt");
+    }
+
+    const challengeId = "red_ch3";
+    let masterSalt;
+
+    if (process.env.CHALLENGE_MASTER_SALT) {
+        masterSalt = process.env.CHALLENGE_MASTER_SALT;
+    } else {
+        context.fail("Expected CHALLENGE_MASTER_SALT env variable to be set.")
+    }
+
+    var verificationHash = crypto.createHash('sha256').update(challengeId + event.salt + masterSalt).digest('base64');
+
+    return context.succeed({
+        verificationCode: verificationHash
+    });
+}
+
