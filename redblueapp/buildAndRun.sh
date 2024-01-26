690e727e10ba830747fdb45375b8036428de2b91
Bilal Khan <khan.bilal88@gmail.com>
2024-01-26 14:56:01 +0530

    Update buildAndRun.sh
    Added command to generate CHALLENGE_MASTER_SALT via openssl.

--- redblueapp/buildAndRun.sh
+++ redblueapp/buildAndRun.sh
@@ -1,2 +1,4 @@
 docker build -t securecodingdojo/redblueapp .
+CHALLENGE_MASTER_SALT=$(openssl rand -hex 16)
+echo $CHALLENGE_MASTER_SALT
 docker run -p 8888:8888 -p 8080:80 -e MASTER_SALT="$CHALLENGE_MASTER_SALT" securecodingdojo/redblueapp
