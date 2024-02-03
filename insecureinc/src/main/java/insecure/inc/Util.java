3ca374ee4c4e4e694393544e14c02e6fce7a7ada
Tim Messing <141575989+timmyteo@users.noreply.github.com>
2024-02-03 11:41:54 -0700

    Adding challenge for CWE-94

--- insecureinc/src/main/java/insecure/inc/Util.java
+++ insecureinc/src/main/java/insecure/inc/Util.java
@@ -31,6 +31,10 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
@@ -81,7 +85,67 @@ public class Util {
 		return sb.toString();
 
 	}
+
+
+	public static String executeJavascript(String command) {
+		if (command == null) {
+			command = "";
+		}
+		
+		String PASSWORD = "Sup3rS3cr3t";
+
+		// permit simple arithmetic expressions
+		Pattern calculatorPattern = Pattern.compile("^[0-9+\\-*\\/]*$");
+    	Matcher calculatorPatternMatcher = calculatorPattern.matcher(command);
+
+		// permit "password"
+		Pattern passwordPattern = Pattern.compile("^password$");
+    	Matcher passwordPatternMatcher = passwordPattern.matcher(command);
+
+		// permit a call to the function deleteHistory, providing the correct password
+		Pattern exploitPattern = Pattern.compile("^deleteHistory\\(['\\\"`]{1}Sup3rS3cr3t['\\\"`]{1}\\);*$");
+    	Matcher exploitPatternMatcher = exploitPattern.matcher(command);
+
+		// permit a call to the function deleteHistory, providing the incorrect password
+		Pattern exploitPatternWrongPassword = Pattern.compile("^deleteHistory\\(['\\\"`]{1}.*['\\\"`]{1}\\);*$");
+    	Matcher exploitPatternWrongPasswordMatcher = exploitPatternWrongPassword.matcher(command);
+
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("JavaScript");
+		String result = "";
+
+		if (exploitPatternMatcher.find()) {
+			return "solved";
+		}
+
+		if (exploitPatternWrongPasswordMatcher.find()) {
+			return command + " = wrong password";
+		}
+
+		// Only allow a limited possibility for executable code to pass through, so as to protect the SecureCodingDojo instance
+    	if (calculatorPatternMatcher.find() || passwordPatternMatcher.find()) {
+			try {
+				// Don't display this in code snippet - they are just dummy functions that ensure the JavaScript code all loads
+				engine.eval("function authenticate(password) { return false; }");
+				engine.eval("function clearDatabase() { return false; }");
+
+				// Do display this in code snippet - it will help to understand how to exploit this challenge
+				engine.eval("var password = '" + PASSWORD + "';");
+				engine.eval("function deleteHistory(password) { if (authenticate(password)) { clearDatabase(); } else { return 'wrong password'; } }");
+				Object outcome = engine.eval(command);
+
+				if (outcome != null) {
+					result = command + " = " + outcome.toString();
+				}
+			} catch (ScriptException e) {
+				System.err.println(e);
+			}
+		}
+
+		return result;
+	}
 	
+
 	public static String bytesToHex(byte[] in) {
 	    final StringBuilder builder = new StringBuilder();
 	    for(byte b : in) {
